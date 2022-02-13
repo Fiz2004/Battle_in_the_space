5ffde248d9fbd2dbe9c5c9eb5079d55159cc569c
Fiz2004 <86916308+Fiz2004@users.noreply.github.com>
2022-02-13 18:29:50 +0700

    refactoring

--- app/src/main/java/com/fiz/android/battleinthespace/base/presentation/options/OptionsFragment.kt
+++ app/src/main/java/com/fiz/android/battleinthespace/base/presentation/options/OptionsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.fiz.android.battleinthespace.R
 import com.fiz.android.battleinthespace.base.data.PlayerRepository
 import com.fiz.android.battleinthespace.base.presentation.MainActivity
 import com.fiz.android.battleinthespace.base.presentation.MainViewModel
@@ -30,36 +31,35 @@ class OptionsFragment : Fragment() {
         binding.mainViewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
-        binding.onePlayer.signUpOut.setOnClickListener {
-            if (binding.onePlayer.signUpOut.text == "Sign_up") {
-                dialogHelper.createSignDialog(DialogHelper.SIGN_UP_STATE)
-            } else {
-                viewModel.user.value = null
-                viewModel.mAuth.signOut()
-                dialogHelper.accHelper.signInOutG()
-            }
+        binding.onePlayer.signUp.setOnClickListener {
+            dialogHelper.createSignDialog(DialogHelper.SIGN_UP_STATE)
+        }
+
+        binding.onePlayer.signOut.setOnClickListener {
+            viewModel.user.value = null
+            viewModel.mAuth.signOut()
+            dialogHelper.accHelper.signInOutG()
         }
 
         binding.onePlayer.signIn.setOnClickListener {
             dialogHelper.createSignDialog(DialogHelper.SIGN_IN_STATE)
         }
 
-        binding.twoPlayer.signUpOut.visibility = View.GONE
+        binding.twoPlayer.signUp.visibility = View.GONE
         binding.twoPlayer.signIn.visibility = View.GONE
+        binding.twoPlayer.signOut.visibility = View.GONE
         binding.twoPlayer.email.visibility = View.GONE
-        binding.threePlayer.signUpOut.visibility = View.GONE
+        binding.threePlayer.signUp.visibility = View.GONE
         binding.threePlayer.signIn.visibility = View.GONE
+        binding.threePlayer.signOut.visibility = View.GONE
         binding.threePlayer.email.visibility = View.GONE
-        binding.fourPlayer.signUpOut.visibility = View.GONE
+        binding.fourPlayer.signUp.visibility = View.GONE
         binding.fourPlayer.signIn.visibility = View.GONE
+        binding.fourPlayer.signOut.visibility = View.GONE
         binding.fourPlayer.email.visibility = View.GONE
 
         viewModel.user.observe(requireActivity()) {
-            if (binding.onePlayer.signUpOut.text == "Sign_up") {
-                uiUpdate(it)
-            } else {
-                uiUpdate(null)
-            }
+            uiUpdate(it)
         }
 
 
@@ -78,14 +78,15 @@ class OptionsFragment : Fragment() {
 
     private fun uiUpdate(user: FirebaseUser?) {
         if (user == null) {
-            binding.onePlayer.email.text = "No email"
-            binding.onePlayer.signUpOut.text = "Sign_up"
-            binding.onePlayer.signIn.text = "Sign_in"
+            binding.onePlayer.email.text = resources.getString(R.string.no_email)
+            binding.onePlayer.signUp.visibility = View.VISIBLE
             binding.onePlayer.signIn.visibility = View.VISIBLE
+            binding.onePlayer.signOut.visibility = View.GONE
         } else {
             binding.onePlayer.email.text = user.email
-            binding.onePlayer.signUpOut.text = "Sign_out"
-            binding.onePlayer.signIn.visibility = View.GONE
+            binding.onePlayer.signUp.visibility = View.VISIBLE
+            binding.onePlayer.signIn.visibility = View.VISIBLE
+            binding.onePlayer.signOut.visibility = View.VISIBLE
         }
     }
 }
