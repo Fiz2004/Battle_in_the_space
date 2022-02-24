40aac67978cb648c3679e5dc71cf40fecc0a81fc
Fiz2004 <86916308+Fiz2004@users.noreply.github.com>
2022-02-24 16:05:13 +0700

    init statistics

--- app/src/main/java/com/fiz/android/battleinthespace/base/presentation/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/fiz/android/battleinthespace/base/presentation/statistics/StatisticsFragment.kt
@@ -5,13 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.fiz.android.battleinthespace.R
+import androidx.lifecycle.ViewModelProvider
+import com.fiz.android.battleinthespace.base.presentation.MainViewModel
+import com.fiz.android.battleinthespace.base.presentation.MainViewModelFactory
+import com.fiz.android.battleinthespace.base.presentation.helpers.StatisticsAdapter
+import com.fiz.android.battleinthespace.databinding.FragmentStatisticsBinding
 
 class StatisticsFragment : Fragment() {
+    private var _binding: FragmentStatisticsBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by lazy {
+        val viewModelFactory = MainViewModelFactory(requireActivity().applicationContext)
+        ViewModelProvider(requireActivity(), viewModelFactory)[MainViewModel::class.java]
+    }
+
+    private lateinit var adapter: StatisticsAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false)
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+
+        initUI()
+
+        return binding.root
+    }
+
+    private fun initUI() {
+        adapter = StatisticsAdapter()
+        binding.statisticsRecyclerView.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.players?.let { adapter.setData(it) }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
