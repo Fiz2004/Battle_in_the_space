8fe18a8c558efaafaca644669a47061093bdf436
faa <Fiz2004@Rambler.ru>
2022-03-01 13:16:21 +0700

    change on DynamicColors

--- app/src/main/java/com/fiz/android/battleinthespace/app/App.kt
+++ app/src/main/java/com/fiz/android/battleinthespace/app/App.kt
@@ -1,11 +1,13 @@
 package com.fiz.android.battleinthespace.app
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import io.realm.Realm
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
         Realm.init(this)
     }
 }
