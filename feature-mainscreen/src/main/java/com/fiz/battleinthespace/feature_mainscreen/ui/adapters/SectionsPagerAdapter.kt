032e5dc0a0179147b62704faf37252792a4475db
faa <Fiz2004@Rambler.ru>
2022-04-26 09:39:30 +0700

    refactoring SectionsPagerAdapter

--- feature-mainscreen/src/main/java/com/fiz/battleinthespace/feature_mainscreen/ui/adapters/SectionsPagerAdapter.kt
+++ feature-mainscreen/src/main/java/com/fiz/battleinthespace/feature_mainscreen/ui/adapters/SectionsPagerAdapter.kt
@@ -4,25 +4,32 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.fiz.battleinthespace.feature_mainscreen.R
 import com.fiz.battleinthespace.feature_mainscreen.ui.mission_selected.MissionSelectedFragment
 import com.fiz.battleinthespace.feature_mainscreen.ui.options.OptionsFragment
 import com.fiz.battleinthespace.feature_mainscreen.ui.space_station.SpaceStationFragment
 import com.fiz.battleinthespace.feature_mainscreen.ui.statistics.StatisticsFragment
 
+const val MISSION_SELECTED_PAGE_INDEX = 0
+const val SPACE_STATION_PAGE_INDEX = 1
+const val STATISTICS_PAGE_INDEX = 2
+const val OPTIONS_PAGE_INDEX = 3
+
 class SectionsPagerAdapter(fm: FragmentManager, lc: Lifecycle) :
-    androidx.viewpager2.adapter.FragmentStateAdapter(fm, lc) {
-    override fun getItemCount(): Int {
-        return 4
-    }
+    FragmentStateAdapter(fm, lc) {
+
+    private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
+        MISSION_SELECTED_PAGE_INDEX to { MissionSelectedFragment() },
+        SPACE_STATION_PAGE_INDEX to { SpaceStationFragment() },
+        STATISTICS_PAGE_INDEX to { StatisticsFragment() },
+        OPTIONS_PAGE_INDEX to { OptionsFragment() }
+    )
+
+    override fun getItemCount() = tabFragmentsCreators.size
 
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
-            0 -> MissionSelectedFragment()
-            1 -> SpaceStationFragment()
-            2 -> StatisticsFragment()
-            else -> OptionsFragment()
-        }
+        return tabFragmentsCreators[position]?.invoke() ?: throw IndexOutOfBoundsException()
     }
 
     companion object {
